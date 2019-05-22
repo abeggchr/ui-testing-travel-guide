d50919c3e8b3031a54fe3f9dc3e8e26d8fb82974
Christian Abegg <christian@abegg.rocks>
2019-05-22 14:38:00 +0200

    added basic c# test

---
+++ samples/tour-of-heroes-net/README.md
@@ -0,0 +1,4 @@
+Tests in C#
+with XUnit
+
+Requires tour-of-heroes to be run in http://localhost:4200
---
+++ samples/tour-of-heroes-net/TourOfHeroesTestNet/BasicTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace TourOfHeroesTestNet
+{
+    public class BasicTest : IDisposable
+    {
+        private readonly IWebDriver _driver;
+
+        public BasicTest()
+        {
+            // Note: using constructor to setup and Dispose() to teardown (XUnit)
+            _driver = new ChromeDriver(".");
+        }
+
+        [Fact]
+        public void A_First_Test()
+        {
+            // open the url
+            _driver.Url = "http://localhost:4200/";
+
+            // search for "Narco"
+            var searchInput = _driver.FindElement(By.CssSelector("#search-box"));
+            searchInput.SendKeys("Narco");
+
+            // wait until UI is updated (don't use Sleep() in a real project)
+            Thread.Sleep(1000);
+
+            // assert there was a single search result found and that it text is Narco
+            var searchResults = _driver.FindElements(By.CssSelector("app-hero-search ul.search-result > li"));
+            Assert.Single(searchResults);
+            var searchResult = searchResults.First();
+            Assert.Equal("Narco", searchResult.Text);
+        }
+
+        public void Dispose()
+        {
+            _driver.Close();
+        }
+    }
+}
+
